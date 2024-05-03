81447843dd90c70dcb69d0912871219b44e620fd
Angel Guillermo de Jesús Pérez Jiménez <140100841+LightDemon12@users.noreply.github.com>
2024-05-02 22:16:36 -0600

    00_Holamundo
    print/comentarios/algunos tipos de variables

---
+++ Practica-Básica/Hola mundo.py
@@ -0,0 +1,39 @@
+# inicio practica del Python 02/05/2024
+
+
+#FORMA DE HACER UN PRINT EN PYTHON---->Comebtario de una sola linea
+print("Hola mundo!")
+
+"""
+la estructura basica de un print es la palabra seguida de 
+parentecis y dentro de ellos comillas dobles 
+dentro de ellas mismas van las palabras 
+que se quieren imprimir en consola
+""" #---->Comentario de varias lineas
+
+
+#los print se pueden usar para programas en consola, lo más básico en si.
+
+print('ejemplo para un pirnt más en consola')
+
+#para poder hacer un salto en un print se usa el comando \n 
+
+print ("Prueba del comando de salto de linea \nprueba correcta!")
+
+#los comentarios se pueden hacer para hacer el codigo más entenbile y para recordarlo luego 
+
+#es recomendable comentar codigo que se crea que no se usa para no perderlo si se necesita en un futuro
+
+#print("Prueba de comentario en codigo") ---> codigo comentado para no perderlo
+
+#para verificar el tipo de dato que se tiene se usa el comando type()
+
+print(type(5)) #variable de tipo int 
+print(type(5.5)) #variable de tipo float
+print(type("prueba")) #variable de tipo string
+print (type(True)) #variable de tipo boolean
+print (type(False)) #variable de tipo boolean
+#las variables booleanas son las que tienen un valor de verdadero o falso siempre van en mayusculas al inicio True o False
+print(type(None)) #variable de tipo None
+print (type(1+5j)) #variable de tipo complejo/complex=imaginario 
+
