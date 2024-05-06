fe9b1cd2056e0c11a6e8367cbb559141e466a896
Angel Guillermo de Jesús Pérez Jiménez <140100841+LightDemon12@users.noreply.github.com>
2024-05-05 22:12:32 -0600

    01_Practica
    Practica de input ,variables y operadores

---
+++ Practica-Básica/Ejercicios/01_Practica.py
@@ -0,0 +1,24 @@
+#ejemplo de uso de operadores
+
+numero1= int(input("Ingrese el primer numero: "))
+numero2= int(input("Ingrese el segundo numero: "))
+
+# Realizar cálculos
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+modulo = numero1 % numero2
+exponente = numero1 ** numero2
+division_entera = numero1 // numero2
+mayor_que = numero1 > numero2
+
+# Mostrar resultados
+print("La suma de los 2 números es:", suma)
+print("La resta de los 2 números es:", resta)
+print("La multiplicación de los 2 números es:", multiplicacion)
+print("La división de los 2 números es:", division)
+print("El módulo de los 2 números es:", modulo)
+print("El exponente de los 2 números es:", exponente)
+print("La división entera de los 2 números es:", division_entera)
+print("¿El número 1 es mayor que el número 2?:", mayor_que)
